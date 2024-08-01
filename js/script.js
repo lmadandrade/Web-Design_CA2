8ce52497450044a068770a370497ac719d157a2a
llucas18guimaraes <149105781+llucas18guimaraes@users.noreply.github.com>
2024-08-01 20:43:12 +0100

    Update script.js
    js update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
-// Contact button
-
 document.addEventListener('DOMContentLoaded', function() {
+    // Contact Pop-Up 
     var popUp = document.getElementById('contactPopUp');
     var btn = document.getElementById('contactBtn');
     var span = document.getElementById('spanForm');
@@ -24,12 +23,35 @@ document.addEventListener('DOMContentLoaded', function() {
         alert("Form submitted successfully!");
         popUp.style.display = 'none';
     }
-});
 
-// JavaScript to add 'loaded' class for fade-in effect
-document.addEventListener("DOMContentLoaded", function() {
+    // FadeIn effect for photos
     var photos = document.querySelectorAll('.photo');
     photos.forEach(function(photo) {
         photo.classList.add('loaded');
+    });
+
+    // photo overlay 
+    var photoContainers = document.querySelectorAll('.photo-container');
+
+    photoContainers.forEach(function(container) {
+        var photo = container.querySelector('.photo');
+        var overlay = container.querySelector('.overlay');
+
+        photo.addEventListener('click', function() {
+            // Hide all overlays
+            document.querySelectorAll('.overlay').forEach(function(ov) {
+                ov.style.display = 'none';
+            });
+
+            // Show the clicked overlay
+            overlay.style.display = 'block';
+        });
+
+        // Hide the overlay when click
+        window.addEventListener('click', function(event) {
+            if (!container.contains(event.target)) {
+                overlay.style.display = 'none';
+            }
+        });
     });
 });
